4cb937c7384d74eb2ec46c87a74b1b705d69ec31
Aditya Chandel <aditya.chandel@aggiemail.usu.edu>
2016-12-10 23:43:53 -0700

    Added geolocation support using google geolocation API

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -4,7 +4,7 @@
 
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('Should generate correct message object', ()=> {
@@ -14,5 +14,18 @@ describe('generateMessage', () => {
 
         expect(message.createdAt).toBeA('number');
         expect(message).toInclude({from, text});
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('Should generate correct location object', () => {
+        var from = 'Aditya';
+        var latitude = 10;
+        var longitude = 20;
+        var url = 'https://www.google.com/maps?q=10,20';
+        var message = generateLocationMessage(from, latitude, longitude);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, url});
     });
 });
